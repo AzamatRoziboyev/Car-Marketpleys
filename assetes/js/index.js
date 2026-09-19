41af4e5e18a5b1d1f36eb05d0f5ff8cc5e3bc2bf
Azamat-Developer <aroziboyev1066@gmail.com>
2026-09-19 23:02:15 +0500

    add

--- assetes/js/index.js
+++ assetes/js/index.js
@@ -2,6 +2,11 @@ const signBtn = document.querySelector(".sign")
 const signUp = document.querySelector(".sign-up")
 const signKatta = document.querySelector(".sign-katta")
 const signClose = document.querySelector(".sign-close")
+// * main2
+const main2Box = document.querySelector(".main2-box")
+const left = document.getElementById("left")
+const right = document.getElementById("right")
+const main2Box1 = document.querySelectorAll(".main2-box1")
 
 signBtn.addEventListener("click", () => {
     signKatta.classList.toggle("active")
@@ -13,5 +18,32 @@ signUp.addEventListener("click", () => {
     signKatta.classList.toggle("active")
 })
 
+// !======main2
+const main2Box1length = main2Box1.length // kartalar uzunligi
+
+
+let hisoblagich = 0;
+
+const cardWidth = 210;
+
+left.addEventListener("click", () => {
+    if(hisoblagich < main2Box1length -1) {
+        hisoblagich++;
+        updateCarousel();
+    }
+})
+right.addEventListener("click", () => {
+    if(hisoblagich > 0) {
+        hisoblagich--;
+        updateCarousel();
+    }
+})
+
+function updateCarousel() {
+    // Yo'lakni chapga surish (minus qiymat)
+    const moveAmount = -hisoblagich * cardWidth
+    // CSS transform  orqali hatrakatlantirish
+    main2Box.style.transform = `translateX(${moveAmount}px)`
+}
 
 
